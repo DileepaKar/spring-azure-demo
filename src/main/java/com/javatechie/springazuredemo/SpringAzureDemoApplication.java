dd7f3ece06c984b2b399a79249a3e8adbe65b272
Dileepa <dileepa.dsk@gmail.com>
2024-04-28 15:46:29 -0700

    Second Commit

--- src/main/java/com/javatechie/springazuredemo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/springazuredemo/SpringAzureDemoApplication.java
@@ -11,7 +11,7 @@ public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
 	public String message(){
-		return "Congratulations";
+		return "Congratulations Dileepa";
 	}
 
 	public static void main(String[] args) {
